fedeca6a94161e6ca0039f0294cf672afaa46dec
Roman Voitalik <voitalik1@gmail.com>
2022-11-01 22:06:24 +0200

    change add method to set method

--- src/main/java/core/mate/academy/service/MachineService.java
+++ src/main/java/core/mate/academy/service/MachineService.java
@@ -1,11 +1,13 @@
 package core.mate.academy.service;
 
+import core.mate.academy.model.Machine;
+
 import java.util.List;
 
 /**
  * Parameterize this service and add its implementation in a separate class.
  */
-public interface MachineService<T> {
+public interface MachineService<T extends Machine> {
 
     /**
      * Return the list of machines.
